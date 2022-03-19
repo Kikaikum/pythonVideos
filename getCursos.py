8a67241210aadb2591d1cf4f22dad414a18481ec
Kikaikum <enriquecabrera92@gmail.com>
2022-03-19 16:14:02 +0100

    0.8

--- getCursos.py
+++ getCursos.py
@@ -1,27 +1,46 @@
 from ipaddress import ip_address
+from pickle import FALSE, TRUE
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 import json
 
+#Coger titulo y URL's de los cursos 
+
+
+"""
+Aqui faltaria ir cambiando automaticamente de pagina
+
+"""
+
+
+
+#Declaracion de variables
 data = {}
+first=TRUE
+
+#Abrir chrome en modo movil 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-
 driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.udemy.com')
-kike=input('mas? ')
 
+#Ir a udemy para iniciar sesion
+driver.get('https://www.udemy.com')
+kike=input('Inicia sesion en udemy y pulsa enter')
+
+#Descargar cursos y preguntar cuando has cambiado de pagina manualmente
 for x in range(2000):
-    kike=input('mas? ')
+    if not first:
+        kike=input('Ves a otra pagina de cursos y pulsa enter')
     for i in range(12):        
         titulo = driver.find_element_by_xpath(f'/html/body/div[1]/div[2]/div/div/ul/li/div[3]/div/div[{i+1}]/div[1]/div/a/strong').text
         print(titulo)        
         url = driver.find_element_by_xpath(f'/html/body/div[1]/div[2]/div/div/ul/li/div[3]/div/div[{i+1}]/div[1]/div/a').get_attribute("href")
         print(url)
-        data[titulo]=url        
+        data[titulo]=url 
+        first=FALSE       
         with open('cursos.json', 'w') as json_file:
             json.dump(data, json_file)
 
--- getUrl.py
+++ getUrl.py
@@ -5,15 +5,24 @@ from selenium.webdriver.chrome.options import Options
 import requests
 import json
 
+#Este archivo a partir de JSON de cursos crea un JSON para cada curso con las URLS de cada video para luego descargarlo
+
+
+#Declarar Variables
 datos=[]
+
+#Abrir Chrome con selenium en modo movil y ah pantalla completa
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
 chrome_options.add_argument('--disable-blink-features=AutomationControlled')
 chrome_options.add_argument("--start-maximized")
-
 driver = webdriver.Chrome(options=chrome_options)
+
+#Ir a udemy y iniciar sesion
 driver.get('https://www.udemy.com')
-kike=input('mas? ')
+kike=input('Inicia sesion en udemy y pulsa enter')
+
+
 
 with open('cursos.json') as file:
     data = json.load(file)
@@ -23,16 +32,20 @@ for i in data:
     driver.get(data[i])
     sleep(2)
     for x in range(50):
-        try:
-            boton = driver.find_element_by_xpath(f'/html/body/div[1]/div[1]/div[1]/div[1]/main/div/div[2]/section/div[2]/div/div/div[{x+1}]')
-            boton.click()
+        try:           
+            boton=driver.find_elements_by_css_selector('udlite-btn.udlite-btn-large.udlite-btn-link.udlite-heading-md.js-panel-toggler.panel--panel-toggler--30J_B')
+            print(boton)
+            for z in boton:
+                print(z)
             print('click1')
-            sleep(10)
+        
+            sleep(5000)
             try:
                 for v in range(20):
-                    boton = driver.find_element_by_xpath(f'/html/body/div[2]/div[1]/div[1]/div[1]/main/div/div[2]/section/div[2]/div/div/div[{x+1}]/div[2]/div/ul/li[{v+1}]')                    
+                    boton = driver.find_element_by_xpath(f'/html/body/div[1]/div[1]/div[1]/div[1]/main/div/div[2]/section/div[2]/div/div/div[7]/div[2]/div/ul/li[{v+1}]')                    
                     boton.click()
                     print('click')
+                    sleep(2)
                     titulo = driver.find_element_by_xpath(f'/html/body/div[2]/div[1]/div/div/main/div/div[2]/section/div[2]/div/div/div[{x+1}]/div[2]/div/ul/li[{v+1}]/div/div[2]/div[1]/div/span/span/span').text
                     print(titulo)
                     link = driver.find_element_by_xpath(f'/html/body/div[2]/div[1]/div[1]/div[1]/main/div/div[1]/div/div/div/div/div/div/div/div/div/section/div/div[1]/div/div/video').get_attribute("src")
